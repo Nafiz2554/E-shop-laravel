d545e0c54e1af01dea02c66daa3b5bf9778ecc0e
Nafiz <fuadnafiz254@gmail.com>
2021-12-11 14:41:19 +0600

    first commit

---
+++ website/resources/views/footer.blade.php
@@ -0,0 +1,8 @@
+<div class="card text-center">
+    <div class="card-body">
+      <h5 class="card-title">Special Order</h5>
+    </div>
+    <div class="card-footer text-muted">
+      within 2 days
+    </div>
+  </div>
